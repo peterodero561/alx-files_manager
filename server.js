eba9c98260e8d297187617747571f76aac13b429
Peter Odero <peterodero561@gmail.com>
2025-01-07 21:37:13 +0300

    update

--- server.js
+++ server.js
@@ -1,15 +1,19 @@
 // Import dependencies
 const express = require('express');
+const bodyParser = require('body-parser');
 const routes = require('./routes/index');
 
 // Create an Express application
 const app = express();
 
-// Set the port
-const PORT = process.env.PORT || 5000;
+// Middleware to parse JSON bodies
+app.use(bodyParser.json());
 
 // Load all routes
 app.use('/', routes);
+
+// Set the port
+const PORT = process.env.PORT || 5000;
 
 // Start the server
 app.listen(PORT, () => {
